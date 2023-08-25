bb33cba22fc44ba6b6db2b4beaf6680c2abc744f
Lelia M <leliamm@uio.no>
2023-08-25 16:17:45 +0200

    first commit

---
+++ MaxSum.java
@@ -0,0 +1,63 @@
+public class MaxSum{
+    public int maxArray(int[] input) {
+        int max = input[0];
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i] > max)
+                max = input[i];
+        }
+
+        return max;
+    }
+
+    public int maxsubSlow(int[] input) {
+        int m = 0; // the maximum found so far
+        for (int j = 0; j < input.length; j++) {
+            for (int k = j; k < input.length; k++) {
+                int s = 0;
+                for (int i = j; i < k; i++) {
+                    s = s + input[i];
+                    if (s > m)
+                        m = s;
+                }
+            }
+        }
+
+        return m;
+    }
+
+    public int maxSubFaster(int[] A) {
+        int n = A.length;
+        int[] prefixSum = new int[n + 1]; // Prefix sum array
+        
+        int S = 0; // the initial prefix sum
+        for (int i = 1; i <= n; i++) {
+            S = S + A[i - 1];
+            prefixSum[i] = S;
+        }
+        
+        int m = 0; // the maximum found so far
+        for (int j = 1; j <= n; j++) {
+            for (int k = j; k <= n; k++) {
+                int s = prefixSum[k] - prefixSum[j - 1];
+                if (s > m) {
+                    m = s;
+                }
+            }
+        }
+        
+        return m;
+    }
+    public int maxSubFastest(int[] A) {
+
+        int mStart = 0; // the initial prefix maximum
+        int mMax = 0;  // the maximum found so far
+        
+        for (int t = 1; t < A.length; t++) {
+            mStart = Math.max(0, mStart + A[t - 1]);
+            mMax = Math.max(mMax, mStart);
+        }
+        
+        return mMax;
+    }
+
+}
---
+++ MaxSumTest.java
@@ -0,0 +1,13 @@
+public class MaxSumTest {
+    public static void main(String [] args){
+         
+          MaxSum maxSum = new MaxSum();
+    int[] A = {1, 2, 3, 4, 5, 6, -10};
+ 
+    System.out.println(maxSum.maxsubSlow(A));
+     System.out.println("Faster " + maxSum.maxSubFaster(A));
+    
+    System.out.println("Fastest " + maxSum.maxSubFastest(A));
+    }
+  
+}
