6c9565340238795534d8d6dd0330e35ae3e47404
Lelia M <leliamm@uio.no>
2023-08-28 08:49:24 +0200

    binary search

---
+++ BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+  public boolean binarySearch(int[] input, int lookingFor){
+    int low = 0;
+    int high = input.length - 1;
+
+    while (low <= high){
+        int i = (low+high)/2; // if double instead of int: double floorValue = Math.floor(divisionResult);
+        if (input[i] == lookingFor){
+            return true;
+        }else if (input[i]<lookingFor){
+            low = i+1;
+        }else if(input[i]>lookingFor){
+            high = i-1;
+        }
+    }
+    return false;
+  }  
+}
