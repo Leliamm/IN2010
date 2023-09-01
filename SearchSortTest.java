4b9a6c411a3da23e8f8dffdb46f42071cbde06f3
Lelia M <leliamm@uio.no>
2023-09-01 15:40:07 +0200

    merge sort

--- SearchSortTest.java
+++ SearchSortTest.java
@@ -1,15 +1,19 @@
 import java.util.Arrays;
 
 public class SearchSortTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Search search = new Search();
         Sort sort = new Sort();
 
-        int[] A = {1,2, 3, 4, 5, 6 , 7, 8};
-        int[] B = {23,22, 13, 4, 55,16 , 7, 28, 0};
+        int[] A = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int[] B = { 23, 22, 13, 4, 55, 16, 7, 28, 0 };
+        int[] C = { 23, 22, 13, 4, 55, 16, 7, 28, 0 };
 
         System.out.println("Binary Search " + search.binarySearch(A, 0));
-        // System.out.println("Bubble sort " + Arrays.toString(sort.bubbleSort(B)));
-        System.out.println("Selection sort " + Arrays.toString(sort.selectionSort(B)));
+        System.out.println("Bubble sort " + Arrays.toString(sort.bubbleSort(C.clone())));
+        int[] F = B.clone();
+        System.out.println("Selection sort " + Arrays.toString(sort.selectionSort(F)));
+        System.out.println("Insertion sort " + Arrays.toString(sort.insertionSort(B)));
+         System.out.println("Merge sort " + Arrays.toString(sort.mergeSort(C.clone())));
     }
 }
--- Sort.java
+++ Sort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Sort {
     public int[] bubbleSort(int[] input) {
-        System.out.println("Bubble sort" + Arrays.toString(input));
+        System.out.println("Bubble sort input" + Arrays.toString(input));
         int temp;
         for (int i = 0; i < input.length - 1; i++) {
             for (int j = 0; j < input.length - i - 1; j++) {
@@ -11,14 +11,14 @@ public class Sort {
                     input[j] = input[j + 1];
                     input[j + 1] = temp;
                 }
-                System.out.println("Bubble sort" + Arrays.toString(input));
+                // System.out.println("Bubble sort" + Arrays.toString(input));
             }
         }
         return input;
     }
 
     public int[] selectionSort(int[] input) {
-        System.out.println("Selection sort" + Arrays.toString(input));
+        System.out.println("Selection sort input" + Arrays.toString(input));
         int temp;
         for (int i = 0; i < input.length; i++) {
             int k = i;
@@ -33,8 +33,76 @@ public class Sort {
                 input[k] = temp;
             }
 
-            System.out.println("Selection sort" + Arrays.toString(input));
+            // System.out.println("Selection sort" + Arrays.toString(input));
         }
         return input;
     }
+
+    public int[] insertionSort(int[] input) {
+        System.out.println("Insertion sort input" + Arrays.toString(input));
+        for (int i = 1; i < input.length; i++) {
+            int j = i;
+            int temp;
+            while (j > 0 && input[j - 1] > input[j]) {
+                temp = input[j - 1];
+                input[j - 1] = input[j];
+                input[j] = temp;
+                j = j - 1;
+                // System.out.println("Insertion sort" + Arrays.toString(input));
+            }
+        }
+        return input;
+    }
+
+    public int[] merge(int[] A, int[] B, int[] C) {
+        int i = 0;
+        int j = 0;
+
+        // System.out.println("Array C input " + Arrays.toString(C));
+        // System.out.println("Array A input " + Arrays.toString(A));
+        // System.out.println("Array B input " + Arrays.toString(B));
+
+        while (i < A.length && j < B.length) {
+            if (A[i] <= B[j]) {
+                C[i + j] = A[i];
+                i = i + 1;
+            } else {
+                C[i + j] = B[j];
+                j = j + 1;
+            }
+        }
+        while (i < A.length) {
+            C[i + j] = A[i];
+            i = i + 1;
+        }
+        while (j < B.length) {
+            C[i + j] = B[j];
+            j = j + 1;
+        }
+        return C;
+    }
+
+    public int[] mergeSort(int[] input) {
+        // System.out.println("Merge sort input" + Arrays.toString(input));
+        if (input.length <= 1) {
+            return input;
+        }
+        int i = input.length / 2;
+        int[] A = new int[i];
+        int[] B = new int[input.length - i];
+        int index = 0;
+
+        for (int j = 0; j < i; j++) {
+            A[j] = input[j];
+        }
+        for (int j = i; j < input.length; j++) {
+            B[index] = input[j];
+            index = index + 1;
+        }
+        mergeSort(A);
+        mergeSort(B);
+
+        return merge(A, B, input);
+    }
+
 }
