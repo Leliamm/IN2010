1524a030316c1dfafa3e9e4de48e0ba86734ecc8
Lelia M <leliamm@uio.no>
2023-11-17 17:19:00 +0100

    tries with autucomplete

--- Trie/Trie.java
+++ Trie/Trie.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Trie {
     Node root;
@@ -22,12 +24,24 @@ public class Trie {
             return children.containsKey(ch);
         }
 
+        public boolean hasChildren() {
+            return !children.isEmpty();
+        }
+
         public void insertNode(char ch) {
             children.put(ch, new Node(ch));
         }
 
         public Node getChild(char ch) {
             return children.get(ch);
+        }
+
+        public Node[] getChildren() {
+            return children.values().toArray(new Node[0]);
+        }
+
+        public void removeChild(char ch) {
+            children.remove(ch);
         }
 
         @Override
@@ -52,14 +66,14 @@ public class Trie {
     }
 
     public boolean contains(String word) {
-        if(word == null) return false;
+        if (word == null)
+            return false;
         char[] chars = word.toCharArray();
         return contains(chars);
     }
 
     private boolean contains(char[] chars) {
         Node current = root;
-
         for (char ch : chars) {
             if (current.hasChild(ch))
                 current = current.getChild(ch);
@@ -68,4 +82,84 @@ public class Trie {
         }
         return current.isEndOfWord;
     }
+
+    //Pre order traversal
+    public void traversePre() {
+        traversePre(root);
+    }
+
+    private void traversePre(Node root) {
+        System.out.println(root.value);
+        //visit children
+        for (Node n : root.getChildren()) {
+            traversePre(n);
+        }
+    }
+
+    //post order traversal
+    public void traversePost() {
+        traversePost(root);
+    }
+
+    private void traversePost(Node root) {
+        for (Node n : root.getChildren()) {
+            traversePost(n);
+        }
+        System.out.println(root.value);
+
+    }
+
+    public void remove(String word) {
+        if (!contains(word))
+            return;
+        remove(root, word, 0);
+    }
+
+    private void remove(Node root, String word, int index) {
+        if (index == word.length()) {
+            root.isEndOfWord = false;
+            return;
+        }
+        char ch = word.charAt(index);
+        Node current = root.getChild(ch);
+        if (current == null)
+            return;
+
+        remove(current, word, index + 1);
+        if (!current.hasChildren() && !current.isEndOfWord) {
+            root.removeChild(ch);
+        }
+    }
+
+    //*********AUTOCOMPLETE METHODS**********
+    private Node findLastNodeOF(String prefix) {
+        Node current = root;
+        for (char ch : prefix.toCharArray()) {
+            Node child = current.getChild(ch);
+            if (child == null)
+                return null;
+            current = child;
+        }
+        return current;
+    }
+
+    public List<String> autocompletion(String prefix) {
+        if(prefix == null) return null;
+        List<String> words = new ArrayList<>();
+        Node lastNode = findLastNodeOF(prefix);
+        findwords(lastNode, prefix, words);
+        return words;
+
+    }
+
+    private void findwords(Node root, String prefix, List<String> words) {
+        if(root == null) return;
+        if (root.isEndOfWord)
+            words.add(prefix);
+
+        for (Node child : root.getChildren()) {
+            findwords(child, prefix + child.value, words);
+        }
+    }
+
 }
--- Trie/TrieTest.java
+++ Trie/TrieTest.java
@@ -10,5 +10,41 @@ public class TrieTest {
         System.out.println(trie.contains("canada"));
         System.out.println("Contains cat (true)");
         System.out.println(trie.contains("cat"));
+
+        System.out.println("Test pre-order traversel ");
+        trie.traversePre();
+        System.out.println("Test post-order traversel ");
+        trie.traversePost();
+
+        System.out.println("Test remove can");
+        trie.remove("can");
+        System.out.println("Contains can? " + trie.contains("can"));
+        System.out.println("Contains canada? " + trie.contains("canada"));
+
+        System.out.println("Add back can and remove canada");
+        trie.insert("can");
+        trie.remove("canada");
+        System.out.println("Contains canada? " + trie.contains("canada"));
+        System.out.println("Contains can? " + trie.contains("can"));
+
+        System.out.println("Test remove null");
+        trie.remove(null);
+        System.out.println("Test remove empty string");
+        trie.remove("");
+
+        System.out.println("Test autocomplet can");
+        trie.insert("canada");
+        trie.insert("candid");
+        trie.insert("candice");
+        System.out.println(trie.autocompletion("can"));
+
+        System.out.println("Test autocomplet candi");
+        System.out.println(trie.autocompletion("candi"));
+
+         System.out.println("Test autocomplet empty string");
+        System.out.println(trie.autocompletion(""));
+          System.out.println("Test autocomplet null");
+        System.out.println(trie.autocompletion(null));
+
     }
 }
